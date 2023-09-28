4b662c3bf09fca61a54e252f5e439476272f38f6
bremea <bremeagames@gmail.com>
2023-09-27 20:06:27 -0400

    logos

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-content: ['./src/**/*.{html,js,svelte,ts}'],
-theme: {
-    extend: {},
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "eerie-black": "#1E1F1F",
+        "outer-space": "#464949",
+        jet: "#323434",
+        aquamarine: "#B5FFE9",
+        "ruddy-blue": "#71A9F7",
+        "ultra-violet": "#F899FF",
+        "bright-pink": "#E76D83",
+      },
+    },
   },
   plugins: [],
-}
-
+};
