1b2084a0c9e4c0ad62a8141a91a62305ec3ca240
bremea <bremeagames@gmail.com>
2023-09-27 21:26:08 -0400

    landing page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex flex-col items-center justify-center w-full h-full min-h-screen">
+	<img src="/logos/logo_128x128.png" alt="Filebunny Logo" />
+	<h1 class="text-bright-pink">filebunny.io</h1>
+</main>
