bb55854207fe504779d515ea0c01018a1f54d8e2
bremea <bremeagames@gmail.com>
2023-09-30 12:43:54 -0400

    update nav design

--- app/src/lib/components/nav/NavItem.svelte
+++ app/src/lib/components/nav/NavItem.svelte
@@ -19,8 +19,8 @@
 </script>
 
 <button
-	class={`group w-full focus:ring-white focus:ring-4 cursor-pointer pr-2 flex items-center relative transition-all px-4 py-2 hover:bg-white hover:bg-opacity-10`}
+	class={`group w-full focus:ring-white focus:ring-2 cursor-pointer pr-2 flex items-center relative transition-all px-4 py-2 hover:bg-white hover:bg-opacity-10 ${active ? 'bg-white bg-opacity-20' : ''}`}
 >
-	<svelte:component this={elements[item]} size="24" class={`${'group-hover:' + iconColors[item]} transition-all ${active ? iconColors[item] : ''}`} />
+	<svelte:component this={elements[item]} size="24" class={`${'group-hover:' + iconColors[item]} align-baseline transition-all ${active ? iconColors[item] : ''}`} />
 	<p class={`${'group-hover:' + textColors[item]} ${active ? 'font-bold' : 'font-normal'} transition-all ml-2 ${active ? textColors[item] : ''}`}>{text[item]}</p>
 </button>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <main class="flex">
 	<div class="flex flex-col items-start py-4 w-48 border-r-2 border-white border-opacity-10 h-screen">
 		<Logo class="px-4 pb-2" />
-		<NavItem item={0} active />
+		<NavItem item={0} />
 		<NavItem item={1} />
 		<NavItem item={2} />
 	</div>
